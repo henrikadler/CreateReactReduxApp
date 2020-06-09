37c715a3439fb1e1ac82bb48b4739f432ff96988
Henrik Adler <henrikadler@users.noreply.github.com>
2020-06-09 10:33:45 +0200

    Add redux provider in test code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import Store from './store'
+
 import App from './App';
 
 it('renders without crashing', () => {
+
+  const initialState = {
+    cart: [],
+    shelfItems: []
+  }
+  const storeInstance = Store(initialState)
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, div);
 });
